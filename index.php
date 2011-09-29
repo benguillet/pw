72a88a29b093ade9e36dc236062a3844f3bbb0b6
Benjamin Guillet <zetartopom@gmail.com>
2011-09-29 13:23:19 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+ <head>
+  <title>Benjamin Guillet - Software Engineering Student</title>
+  <?php
+   include('./includes/metas.inc.html');
+   include('./includes/links.inc.html');
+   include('./includes/scripts.inc.html');
+  ?>
+ </head>
+ <body>
+  <div id="main">
+   <h1><a href="/" title="Home"><span class="firstname">benjamin</span>guillet</a></h1>
+    <dl class="menu">
+      <dt><a href="about.php">about</a></dt>
+      <dd id="about">All about me</dd>
+
+      <dt><a href="projets.php">projects</a></dt>
+      <dd id="projects">My projects</dd>
+    </dl>
+   <p id="footer"></p>
+  </div>
+ </body>
+</html>
---
+++ projects.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+ <head>
+  <title>Benjamin Guillet - Projects</title>
+  <?php
+   include('./includes/metas.inc.html');
+   include('./includes/links.inc.html');
+   include('./includes/scripts.inc.html');
+  ?>
+ </head>
+ <body>
+  <div id="main">
+   <h1><a href="/" title="Home">p<span class="firstname">rojects</span></a></h1>
+   <p>Work in progress...</p>
+   <p id="footer"></p>
+  </div>
+ </body>
+</html>
