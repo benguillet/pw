cf148556f93dec75d43662c4da3d011faadf5ee8
Benjamin Guillet <zetartopom@gmail.com>
2011-10-09 18:39:35 +0200

    Changement dans le style

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         <link rel="stylesheet" media="all" href="./styles/chocolate/screen.css" />    
     </head>
     <body>
+        <div id="bar"></div>
         <header role="banner">
             <h1><a href="/" title="Home"><span class="firstname">benjamin</span>guillet</a></h1>        
         </header>
