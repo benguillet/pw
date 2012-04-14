7188df5e8231f00371fffb3b5c8ae28ac2a67a16
Benjamin Guillet <benjamin.guillet@gmail.com>
2012-04-13 23:38:57 -0700

    complete redesign

--- index.php
+++ index.php
@@ -6,17 +6,59 @@
         <title>Benjamin Guillet - Software Engineering Student</title>
         <meta name="author" content="Benjamin Guillet" />        
         <!--[if IE]><script src="scripts/html5-ie.js"></script><![endif]-->
-        <link rel="stylesheet" media="all" href="/styles/screen.css" />
+        <link href='http://fonts.googleapis.com/css?family=Terminal+Dosis:500' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" media="all" href="styles/screen.css" />
+        <link rel="stylesheet" href="styles/proudify.min.css" type="text/css"/>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+        <script type="text/javascript" src="https://raw.github.com/icebreaker/proudify/master/jquery-proudify.min.js"></script>
+        <script type="text/javascript" src="scripts/jquery-scorecard.min.js"></script>
+
+    </head>
     </head>
     <body>
-        <header role="banner">
-            <h1><a href="/" title="Home"><span class="firstname">benjamin</span>guillet</a></h1>        
-        </header>
-        <nav role="navigation">
-            <ul>
-                <li><a href="./about.php">about</a></li>
-                <li><a href="./projets.php">projects</a></li>
-            </ul>
-        </nav>
+        <div id="main" role="main">
+            <header role="banner">
+                <h1><a href="/" title="Home"><span class="firstname">Benjamin</span>Guillet</a></h1>
+                <h2>Software <span class="important">Engineering</span> Student</h2>        
+            </header>
+            <h3>About</h3>
+            <h3>Social</h3>
+            <a title="Linkedin" target="_blank" class="social linkedin" href="http://www.linkedin.com/in/benjaminguillet/en">http://www.linkedin.com/in/benjaminguillet/en</a>
+        </div>
+
+
+        <!--<h2>GitHub Projects</h2>
+        <div id="github"></div>
+        <h2>Coderwall Achievement Badges</h2>
+        <div id="coderwall"></div>
+        <h2>Codeschool Badges</h2>
+        <h3>Completed courses</h3>
+        <div id="cs-completed">
+            <p>All the courses that I've aced!</p>
+        </div>
+        <h3>Courses in Progress</h3>
+        <div id="cs-in-progress">
+            <p>Working on them...</p>
+        </div>
+        <script type="text/javascript">
+            $('#github').proudify({'username':'Tartopom', 'num':'0'});
+            $('#coderwall').proudify({'username':'tartopom', 'service':'coderwall'});
+            $('#cs-completed').scorecard({
+                username: 'Tartopom',
+                sections: ['completed'],
+                newtab: true,
+                loading: {
+                  message: 'Loading Code School badges...'
+                }
+            });
+            $('#cs-in-progress').scorecard({
+                username: 'Tartopom',
+                sections: ['in-progress'],
+                newtab: true,
+                loading: {
+                  message: 'Loading Code School badges...'
+                }
+            });
+        </script>-->
     </body>
 </html>
---
+++ scripts/cs-badges.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#my-cs-badges').scorecard({
+        username: 'Tartopom',
+        sections: ['badges'],
+        loading: message: 'Loading Code School badges...'
+    });
+});
