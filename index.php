0987eb6f04bef06fa50d84786cbc8bbd1587f066
Benjamin Guillet <benjamin.guillet@gmail.com>
2012-04-14 01:03:40 -0700

    in good progress...

--- index.php
+++ index.php
@@ -7,25 +7,34 @@
         <meta name="author" content="Benjamin Guillet" />        
         <!--[if IE]><script src="scripts/html5-ie.js"></script><![endif]-->
         <link href='http://fonts.googleapis.com/css?family=Terminal+Dosis:500' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" media="all" href="styles/screen.css" />
         <link rel="stylesheet" href="styles/proudify.min.css" type="text/css"/>
+        <link rel="stylesheet" media="all" href="styles/screen.css" />
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
         <script type="text/javascript" src="https://raw.github.com/icebreaker/proudify/master/jquery-proudify.min.js"></script>
-        <script type="text/javascript" src="scripts/jquery-scorecard.min.js"></script>
-
+        <script type="text/javascript" src="scripts/menu.min.js"></script>
     </head>
     </head>
     <body>
-        <div id="main" role="main">
-            <header role="banner">
+        <header role="banner">
                 <h1><a href="/" title="Home"><span class="firstname">Benjamin</span>Guillet</a></h1>
                 <h2>Software <span class="important">Engineering</span> Student</h2>        
-            </header>
-            <h3>About</h3>
-            <h3>Social</h3>
-            <a title="Linkedin" target="_blank" class="social linkedin" href="http://www.linkedin.com/in/benjaminguillet/en">http://www.linkedin.com/in/benjaminguillet/en</a>
+        </header>
+        <div id="content">
+            <h3 id="about">About</h3>
+            <div id="presentation">
+                <p>Hi!</p>
+                <p>I'm a 21 years old Software Engineering student at 
+                the <a href="http://www.utbm.fr" target="_blank">University of Technology of Belfort-Montbéliard</a> in France, 
+                and French Teacher Assistant soon-to-be at <a href="http://stolaf.edu/depts/french/activities/activity.html" target="_blank">St. Olaf College</a>, 
+                as well as an exchange student there.</p>
+                <p>I'm currently doing my internship in Venice, CA in a Mobile Marketing startup called 
+                <a href="http://www.mogreet.com" target="_blank">Mogreet, Inc</a>, with an incredible atmosphere and technology use. 
+                I love California and the way of life here.<p/>
+            </div>
+            <h3 id="projects">Projects</h3>
+            <div id="github"></div>
+            <h3><a href="http://blog.benjaminguillet.com" target="_blanck">Blog</a></h3>
         </div>
-
 
         <!--<h2>GitHub Projects</h2>
         <div id="github"></div>
